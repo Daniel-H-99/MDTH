ecbff5a7734cb9823f06eb031c1165ca4418c043
Daniel-H-99 <myhwang99@kaist.ac.kr>
2022-09-14 11:47:00 +0900

    fixing

--- models/fv2v2/run.py
+++ models/fv2v2/run.py
@@ -29,6 +29,8 @@ if __name__ == "__main__":
     parser.add_argument("--gen", default="original", choices=["original", "spade"])
     parser.add_argument("--log_dir", default='log', help="path to log into")
     parser.add_argument("--checkpoint", default=None, help="path to checkpoint to restore")
+    parser.add_argument("--checkpoint_ref", default='00000189-checkpoint.pth.tar', help="path to checkpoint to restore")
+    parser.add_argument("--pretrained_pose", action='store_true', help="path to checkpoint to restore")
     parser.add_argument("--device_ids", default="0, 1, 2, 3, 4, 5, 6, 7", type=lambda x: list(map(int, x.split(','))),
                         help="Names of the devices comma separated.")
     parser.add_argument("--verbose", dest="verbose", action="store_true", help="Print model architecture")

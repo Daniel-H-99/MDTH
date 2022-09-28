5420a92061db3e00c1d49870e0efde90e6c6a0d7
Daniel-H-99 <myhwang99@kaist.ac.kr>
2022-09-28 23:32:47 +0900

    initialized

--- models/fv2v2/modules/keypoint_detector.py
+++ models/fv2v2/modules/keypoint_detector.py
@@ -210,9 +210,10 @@ class ExpTransformer(nn.Module):
 
     def split_embedding(self, img_embedding):
         style_embedding, exp_embedding = img_embedding.split([self.latent_dim // 2, self.latent_dim // 2], dim=1)
-        exp_embedding = self.vq_exp(exp_embedding)
+        exp_embedding = self.vq_exp(exp_embedding)  # B x num_heads
+        exp_embedding = torch.einsum('bk,kp->bkp', exp_embedding, F.normalize(self.codebook))
         exp_embedding = exp_embedding.flatten(1)
-        exp_embedding = self.codebook(exp_embedding)
+        
         return {'style': style_embedding, 'exp': exp_embedding}
 
     def fuse(self, style, exp):

9c5f643b85b5155bf729165d46447051553b4723
seohuibae <seohuibae73@gmail.com>
2022-10-20 05:08:08 +0900

    ckpt: meeting 10.4

--- models/fv2v2/modules/model.py
+++ models/fv2v2/modules/model.py
@@ -1736,7 +1736,7 @@ class ExpTransformerTrainer(GeneratorFullModelWithSeg):
                 # print(f'motion_section: {motion_section}')
                 # print(f'motion_section_GT : {motion_GT}')
                 
-                loss_values['motion_match'] = 0 * self.loss_weights['motion_match'] * F.l1_loss(motion_section, motion_GT) \
+                loss_values['motion_match'] = 1 * self.loss_weights['motion_match'] * F.l1_loss(motion_section, motion_GT) \
                                                 + 10 * self.loss_weights['motion_match'] * F.l1_loss(motion_section_eye, motion_GT_eye) \
                                                 + 10 * self.loss_weights['motion_match'] * F.l1_loss(motion_section_mouth, motion_GT_mouth)
 

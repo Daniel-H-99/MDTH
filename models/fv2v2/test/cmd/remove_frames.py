fca8b71353061c017727e404a70467a7ebe6b10e
seohuibae <seohuibae73@gmail.com>
2022-11-13 11:46:50 +0900

    add: evaluation

--- models/fv2v2/test/cmd/remove_frames.py
+++ models/fv2v2/test/cmd/remove_frames.py
@@ -9,7 +9,7 @@ tgt_dir = '/home/server19/minyeong_workspace/MDTH/models/fv2v2/test/res/'
 
 dirs = []
 for dir in os.listdir(tgt_dir):
-    if dir.startswith('0_'):
+    if 's1' in dir:
         dirs.append(os.path.join(tgt_dir, dir))
      
 for dir in dirs:

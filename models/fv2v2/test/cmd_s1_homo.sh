32c3f0e5df68ce668c0f85a94a9468bf0fa68489
seohuibae <seohuibae73@gmail.com>
2022-11-13 11:51:47 +0900

    add: evaluation

--- models/fv2v2/test/cmd_s1_homo.sh
+++ models/fv2v2/test/cmd_s1_homo.sh
@@ -1,4 +1,3 @@
-CUDA_VISIBLE_DEVICES=1 python test.py --config config/config_homo_0_s1.yaml
 CUDA_VISIBLE_DEVICES=1 python test.py --config config/config_homo_1_s1.yaml
 CUDA_VISIBLE_DEVICES=1 python test.py --config config/config_homo_2_s1.yaml
 CUDA_VISIBLE_DEVICES=1 python test.py --config config/config_homo_3_s1.yaml

2d9eb0e75d4e10259d42861f6130d43e2a0413df
seohuibae <seohuibae73@gmail.com>
2022-11-28 03:27:02 +0900

    evaluated

--- models/fv2v2/test/cmd/concat_videos.py
+++ models/fv2v2/test/cmd/concat_videos.py
@@ -1,25 +1,37 @@
 import os
 import shutil
 import numpy as np
+from tqdm import tqdm
 
 root_dir = 'demos'
 baseline_root_dir = os.path.join(root_dir,'baseline')
-baselines = ['MMRA','TPSMM', 'LIA', 'OURS', 'OURS_NOLOG']
-output_root_dir = os.path.join(baselines_root_dir, 'output')
+# baselines = ['MMRA','TPSMM', 'LIA', 'OURS', 'OURS_NOLOG']
+baselines = ['TPSMM', 'LIA', 'OURS']
+output_root_dir = os.path.join(root_dir, 'output')
 output_video_dir = os.path.join(output_root_dir, 'video')
 label_file_path = os.path.join(output_root_dir, 'labels.txt')
+ROOT = '/home/server19/minyeong_workspace/MDTH/models/fv2v2/test'
 
-def construct_video_list(vid_dir):
+def construct_video_list(baseline_dir):
     res = []
-    group_dirs = os.listdir(vid_dir)
+    src_dict = {}
+    drv_dict = {}
+    group_dirs = os.listdir(baseline_dir)
     for g in group_dirs:
         print(f'working on group: {g}')
-        g_dir = os.path.join(vid_dir, g)
+        g_dir = os.path.join(baseline_dir, g)
         vids = os.listdir(g_dir)
+        vids = list(filter(lambda x: '.mp4' in x, vids))
+        for vid in vids:
+            vid_dir = os.path.join(g_dir, vid)
+            inputs= np.loadtxt(os.path.join(vid_dir, 'inputs.txt'), dtype=str, comments=None)
+            src_path, drv_path = inputs
+            src_dict[vid] = src_path
+            drv_dict[vid] = drv_path
         res.extend(vids)
 
     print(f'total videos in list: {len(res)}')
-    return res
+    return res, src_dict, drv_dict
 
 def construct_video_dict(vid_list):
     res = {}
@@ -30,16 +42,17 @@ def construct_video_dict(vid_list):
         groups = os.listdir(baseline_dir)
         print(f'found {len(groups)} groups')
         for g in groups:
-            g_dir = os.path.join(baselin_dir, g)
+            g_dir = os.path.join(baseline_dir, g)
             vids = os.listdir(g_dir)
+            vids = list(filter(lambda x: '.mp4' in x, vids))
             for vid in vids:
                 if vid in vid_list:
-                    vid_path = os.path.join(g_dir, vid)
+                    vid_path = os.path.join(g_dir, vid, 'mute.mp4')
                     res_baseline[vid] = vid_path
         res[baseline] = res_baseline
 
     cnt = -1
-    for k, v in res.item():
+    for k, v in res.items():
         if cnt == -1:
             cnt = len(list(v.keys()))
         else:
@@ -50,30 +63,61 @@ def construct_video_dict(vid_list):
 def get_src_drv_path(src_dir, drv_dir):
     src_frame_path = os.path.join(src_dir, 'frames', '0000000.png')
     drv_frames_dir = os.path.join(drv_dir, 'frames')
-    NotImplementedError
+    video_path = os.path.join(drv_dir, 'video.mp4')
+    if not os.path.exists(video_path):
+        cmd = [f'cd {drv_frames_dir}']
+        cmd.append(f'ffmpeg -y -framerate 25 -i %07d.png -c:v libx264 -r 25 {video_path}')
+        cmd.append(f'cd {ROOT}')
+        os.system(';'.join(cmd))
+        print(f'cmd: {cmd}')
+    return src_frame_path, video_path
+
 
 def make_labels(vid_list):
     N = len(vid_list)
     K = len(baselines)
-    random_order = np.concatenate([np.random.permutation(baselines) for _ in range(N)], axis=0)
+    random_order = np.stack([np.random.permutation(baselines) for _ in range(N)], axis=0)
     return random_order
 
 def mp4_to_gif(path):
-    NotImplementedError
-
+    basename = os.path.basename(path)
+    output_basename = basename[:-4] + '.gif'
+    output_path = os.path.join(os.path.dirname(path), output_basename)
+    cmd = f'ffmpeg -y -i {path} -r 25 {output_path}'
+    os.system(cmd)
+    return cmd
 
 def make_concat_video_cmd(output_name, src_path, drv_path, result_path_list):
-    cmd = f'ffmpeg'
-    cwd = output_video_dir
-	output_video = os.path.join(cwd, output_name)
+    output_video = output_name
     concat_src_list = [src_path, drv_path] + result_path_list
-	concat_cmd = ' -i '.join(concat_src_list)
-	cmd = f'ffmpeg -i {concat_cmd} -filter_complex hstack=inputs={len(concat_src_list)} -y "{output_video}"'
-	print(f'cmd: {cmd}')
+    concat_src_list = [f'"{v}"' for v in concat_src_list]
+    concat_cmd = ' -i '.join(concat_src_list)
+    cmd = f'ffmpeg -y -i {concat_cmd} -filter_complex hstack=inputs={len(concat_src_list)} -y "{output_video}"'
+    print(f'cmd: {cmd}')
     return cmd
 
 def main():
+    print(f'started')
+    ours_dir = os.path.join(baseline_root_dir, 'OURS')
+    video_list, src_dict, drv_dict = construct_video_list(ours_dir)
+    video_dict = construct_video_dict(video_list)
+    print(f'video_list_sample: {video_list[:5]}')
+    labels = make_labels(video_list)
+    print(f'label samples: {labels[:5]}')
+    np.savetxt(label_file_path, labels, fmt="%s", comments=None)
+    for vid, label in tqdm(zip(video_list, labels)):
+        print(f'woring on vid - label: {vid} - {label}')
+        src_dir = src_dict[vid]
+        drv_dir = drv_dict[vid]
+        src_path, drv_path = get_src_drv_path(src_dir, drv_dir)
+        results = [video_dict[baseline][vid] for baseline in label]
+        output_name = os.path.join(output_video_dir, vid + '.mp4')
+        concat_cmd = make_concat_video_cmd(output_name, src_path, drv_path, results)
+        os.system(concat_cmd)
+        gif_cmd = mp4_to_gif(output_name)
+    print(f'Done')
 
-
+if __name__=='__main__':
+    main()
 
     
---
+++ models/fv2v2/test/cmd_nolog_hetero.sh
@@ -0,0 +1,5 @@
+CUDA_VISIBLE_DEVICES=0 python test.py --config config/config_hetero_0_nolog.yaml
+CUDA_VISIBLE_DEVICES=0 python test.py --config config/config_hetero_1_nolog.yaml
+CUDA_VISIBLE_DEVICES=0 python test.py --config config/config_hetero_2_nolog.yaml
+CUDA_VISIBLE_DEVICES=0 python test.py --config config/config_hetero_3_nolog.yaml
+CUDA_VISIBLE_DEVICES=0 python test.py --config config/config_hetero_4_nolog.yaml

a9a0f4484579a574a36272032fc573afe1f8f005
seohuibae <seohuibae73@gmail.com>
2022-11-30 17:37:42 +0900

    updated concate_videos

--- models/fv2v2/test/cmd/concat_videos.py
+++ models/fv2v2/test/cmd/concat_videos.py
@@ -6,10 +6,11 @@ from tqdm import tqdm
 root_dir = 'demos'
 baseline_root_dir = os.path.join(root_dir,'baseline_k-actors')
 # baselines = ['MMRA','TPSMM', 'LIA', 'OURS', 'OURS_NOLOG']
-baselines = ['FT', '3.2.1']
+baselines = ['FT', '3.2.1', 'OURS_NOLOG']
 output_root_dir = os.path.join(root_dir, 'output_k-actors_ours')
 output_video_dir = os.path.join(output_root_dir, 'video')
 label_file_path = os.path.join(output_root_dir, 'labels.txt')
+list_file_path = os.path.join(output_root_dir, 'videos.txt')
 ROOT = '/home/server19/minyeong_workspace/MDTH/models/fv2v2/test'
 os.makedirs(output_root_dir, exist_ok=True)
 os.makedirs(output_video_dir, exist_ok=True)
@@ -69,12 +70,14 @@ def get_src_drv_path(src_dir, drv_dir):
         src_frame_path = os.path.join(src_dir, 'image.png')
     drv_frames_dir = os.path.join(drv_dir, 'frames')
     video_path = os.path.join(drv_dir, 'video.mp4')
-    if not os.path.exists(video_path):
-        cmd = [f'cd {drv_frames_dir}']
-        cmd.append(f'ffmpeg -y -framerate 25 -i %07d.png -c:v libx264 -r 25 {video_path}')
-        cmd.append(f'cd {ROOT}')
-        os.system(';'.join(cmd))
-        print(f'cmd: {cmd}')
+    src_frame_path = src_frame_path.replace('%d', '%_d')
+    video_path = video_path.replace('%d', '%_d')
+    # if not os.path.exists(video_path):
+        # cmd = [f'cd {drv_frames_dir}']
+        # cmd.append(f'ffmpeg -y -framerate 25 -i %07d.png -c:v libx264 -r 25 {video_path}')
+        # cmd.append(f'cd {ROOT}')
+        # os.system(';'.join(cmd))
+        # print(f'cmd: {cmd}')
     return src_frame_path, video_path
 
 
@@ -94,7 +97,7 @@ def mp4_to_gif(path):
 
 def make_concat_video_cmd(output_name, src_path, drv_path, result_path_list):
     output_video = output_name
-    concat_src_list = [src_path, drv_path] + result_path_list
+    concat_src_list = [drv_path, src_path] + result_path_list
     concat_src_list = [f'"{v}"' for v in concat_src_list]
     concat_cmd = ' -i '.join(concat_src_list)
     cmd = f'ffmpeg -y -i {concat_cmd} -filter_complex hstack=inputs={len(concat_src_list)} -y "{output_video}"'
@@ -110,13 +113,15 @@ def main():
     labels = make_labels(video_list)
     print(f'label samples: {labels[:5]}')
     np.savetxt(label_file_path, labels, fmt="%s", comments=None)
-    for vid, label in tqdm(zip(video_list, labels)):
+    np.savetxt(list_file_path, video_list, fmt="%s", comments=None)
+
+    for i, (vid, label) in tqdm(enumerate(zip(video_list, labels))):
         print(f'woring on vid - label: {vid} - {label}')
         src_dir = src_dict[vid]
         drv_dir = drv_dict[vid]
         src_path, drv_path = get_src_drv_path(src_dir, drv_dir)
         results = [video_dict[baseline][vid] for baseline in label]
-        output_name = os.path.join(output_video_dir, vid + '.mp4')
+        output_name = os.path.join(output_video_dir, '{:02d}_'.format(i) + vid + '.mp4')
         concat_cmd = make_concat_video_cmd(output_name, src_path, drv_path, results)
         os.system(concat_cmd)
         gif_cmd = mp4_to_gif(output_name)

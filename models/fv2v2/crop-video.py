f81047df1906f1e36f89b113768a1e4279cb95ef
seohuibae <seohuibae73@gmail.com>
2022-12-22 13:46:23 +0900

    latest

--- models/fv2v2/crop-video.py
+++ models/fv2v2/crop-video.py
@@ -9,7 +9,23 @@ import os
 import imageio
 import numpy as np
 import warnings
+import os
+
 warnings.filterwarnings("ignore")
+
+from ffhq_align import image_align
+
+def process_image(inp, output):
+    # image align
+    # print(f'image shape: {inp.shape}')
+
+    landmarks_detector = face_alignment.FaceAlignment(face_alignment.LandmarksType._3D, flip_input=False)
+
+    for i, face_landmarks in enumerate(landmarks_detector.get_landmarks(inp), start=1):
+        aligned_face_path = output
+        result_img = image_align(inp, face_landmarks)
+        result_img.save(aligned_face_path, 'PNG')
+
 
 def extract_bbox(frame, fa):
     if max(frame.shape[0], frame.shape[1]) > 640:
@@ -45,12 +61,50 @@ def join(tube_bbox, bbox):
     yB = max(tube_bbox[3], bbox[3])
     return (xA, yA, xB, yB)
 
+def process_frame(inp, output, fa):
+    frame = imageio.imread(inp)
+    bbox = extract_bbox(frame, fa)[0]
+    compute_bbox_image(bbox, frame.shape, inp, output, increase_area=0.1)
 
-def compute_bbox(start, end, fps, tube_bbox, frame_shape, inp, image_shape, increase_area=0.1):
+def compute_bbox_image(bbox, frame_shape, inp, output, increase_area=0.1):
+    left, top, right, bot = bbox
+    width = right - left
+    height = bot - top
+
+    
+    #Computing aspect preserving bbox
+    width_increase = max(increase_area, ((1 + 2 * increase_area) * height - width) / (2 * width))
+    height_increase = max(increase_area, ((1 + 2 * increase_area) * width - height) / (2 * height))
+    # width_increase = max(increase_area, 3 * increase_area)
+    # height_increase = max(increase_area, 3 * increase_area )
+    print(f'width; {width}')
+    print(f'height; {height}')
+    left = int(left - width_increase * width)
+    top = int(top - height_increase * height)
+    right = int(right + width_increase * width)
+    bot = int(bot + height_increase * height)
+
+    top, bot, left, right = max(0, top), min(bot, frame_shape[0]), max(0, left), min(right, frame_shape[1])
+    h, w = bot - top, right - left
+
+
+    
+    scale = '256:256'
+
+    scale_file_path = output[:-4] + '.txt'
+    np.savetxt(scale_file_path, np.array([h, w]), delimiter=',', comments=None)
+    cmd = f'ffmpeg -y -i {inp} -filter:v "fps=25, crop={w}:{h}:{left}:{top}, scale={scale}" {output}'
+    print(f'cmd: {cmd}')
+    os.system(cmd)
+    return 
+
+
+def compute_bbox(start, end, fps, tube_bbox, frame_shape, inp, image_shape, output, increase_area=0.1):
     left, top, right, bot = tube_bbox
     width = right - left
     height = bot - top
 
+    
     #Computing aspect preserving bbox
     width_increase = max(increase_area, ((1 + 2 * increase_area) * height - width) / (2 * width))
     height_increase = max(increase_area, ((1 + 2 * increase_area) * width - height) / (2 * height))
@@ -69,14 +123,14 @@ def compute_bbox(start, end, fps, tube_bbox, frame_shape, inp, image_shape, incr
 
     scale = f'{image_shape[0]}:{image_shape[1]}'
 
-    return f'ffmpeg -i {inp} -ss {start} -t {time} -filter:v "crop={w}:{h}:{left}:{top}, scale={scale}" crop.mp4'
+    return f'ffmpeg -y -i {inp} -ss {start} -t {time} -filter:v "fps=25, crop={w}:{h}:{left}:{top}, scale={scale}" {output}'
 
 
 def compute_bbox_trajectories(trajectories, fps, frame_shape, args):
     commands = []
     for i, (bbox, tube_bbox, start, end) in enumerate(trajectories):
         if (end - start) > args.min_frames:
-            command = compute_bbox(start, end, fps, tube_bbox, frame_shape, inp=args.inp, image_shape=args.image_shape, increase_area=args.increase)
+            command = compute_bbox(start, end, fps, tube_bbox, frame_shape, inp=args.inp, image_shape=args.image_shape, increase_area=args.increase, output=args.output)
             commands.append(command)
     return commands
 
@@ -85,56 +139,61 @@ def process_video(args):
     device = 'cpu' if args.cpu else 'cuda'
     fa = face_alignment.FaceAlignment(face_alignment.LandmarksType._2D, flip_input=False, device=device)
     video = imageio.get_reader(args.inp)
+    if 'fps' not in video.get_meta_data():
+        # process_image(inp=args.inp, output=args.output)
+        process_frame(args.inp, args.output, fa)
+        return []
+    else:
+        trajectories = []
+        previous_frame = None
+        fps = video.get_meta_data()['fps']
+        commands = []
+        try:
+            for i, frame in tqdm(enumerate(video)):
+                frame_shape = frame.shape
+                bboxes =  extract_bbox(frame, fa)
+                ## For each trajectory check the criterion
+                not_valid_trajectories = []
+                valid_trajectories = []
 
-    trajectories = []
-    previous_frame = None
-    fps = video.get_meta_data()['fps']
-    commands = []
-    try:
-        for i, frame in tqdm(enumerate(video)):
-            frame_shape = frame.shape
-            bboxes =  extract_bbox(frame, fa)
-            ## For each trajectory check the criterion
-            not_valid_trajectories = []
-            valid_trajectories = []
-
-            for trajectory in trajectories:
-                tube_bbox = trajectory[0]
-                intersection = 0
-                for bbox in bboxes:
-                    intersection = max(intersection, bb_intersection_over_union(tube_bbox, bbox))
-                if intersection > args.iou_with_initial:
-                    valid_trajectories.append(trajectory)
-                else:
-                    not_valid_trajectories.append(trajectory)
-
-            commands += compute_bbox_trajectories(not_valid_trajectories, fps, frame_shape, args)
-            trajectories = valid_trajectories
-
-            ## Assign bbox to trajectories, create new trajectories
-            for bbox in bboxes:
-                intersection = 0
-                current_trajectory = None
                 for trajectory in trajectories:
                     tube_bbox = trajectory[0]
-                    current_intersection = bb_intersection_over_union(tube_bbox, bbox)
-                    if intersection < current_intersection and current_intersection > args.iou_with_initial:
-                        intersection = bb_intersection_over_union(tube_bbox, bbox)
-                        current_trajectory = trajectory
+                    intersection = 0
+                    for bbox in bboxes:
+                        intersection = max(intersection, bb_intersection_over_union(tube_bbox, bbox))
+                    if intersection > args.iou_with_initial:
+                        valid_trajectories.append(trajectory)
+                    else:
+                        not_valid_trajectories.append(trajectory)
 
-                ## Create new trajectory
-                if current_trajectory is None:
-                    trajectories.append([bbox, bbox, i, i])
-                else:
-                    current_trajectory[3] = i
-                    current_trajectory[1] = join(current_trajectory[1], bbox)
+                commands += compute_bbox_trajectories(not_valid_trajectories, fps, frame_shape, args)
+                trajectories = valid_trajectories
+
+                ## Assign bbox to trajectories, create new trajectories
+                for bbox in bboxes:
+                    intersection = 0
+                    current_trajectory = None
+                    for trajectory in trajectories:
+                        tube_bbox = trajectory[0]
+                        current_intersection = bb_intersection_over_union(tube_bbox, bbox)
+                        if intersection < current_intersection and current_intersection > args.iou_with_initial:
+                            intersection = bb_intersection_over_union(tube_bbox, bbox)
+                            current_trajectory = trajectory
+
+                    ## Create new trajectory
+                    if current_trajectory is None:
+                        trajectories.append([bbox, bbox, i, i])
+                    else:
+                        current_trajectory[3] = i
+                        current_trajectory[1] = join(current_trajectory[1], bbox)
 
 
-    except IndexError as e:
-        raise (e)
+        except Exception as e:
+            print (e)
 
-    commands += compute_bbox_trajectories(trajectories, fps, frame_shape, args)
-    return commands
+        commands += compute_bbox_trajectories(trajectories, fps, frame_shape, args)
+
+        return commands
 
 
 if __name__ == "__main__":
@@ -145,14 +204,18 @@ if __name__ == "__main__":
     parser.add_argument("--increase", default=0.1, type=float, help='Increase bbox by this amount')
     parser.add_argument("--iou_with_initial", type=float, default=0.25, help="The minimal allowed iou with inital bbox")
     parser.add_argument("--inp", required=True, help='Input image or video')
-    parser.add_argument("--min_frames", type=int, default=150,  help='Minimum number of frames')
+    parser.add_argument("--output", default="crop.mp4", help='output name')
+    parser.add_argument("--min_frames", type=int, default=1,  help='Minimum number of frames')
     parser.add_argument("--cpu", dest="cpu", action="store_true", help="cpu mode.")
 
 
     args = parser.parse_args()
 
     commands = process_video(args)
-    for command in commands:
-        print (command)
+    print(f'Commands: {len(commands)}')
+    print(commands)
+    if commands is not None:
+        for command in commands:
+            os.system(command)
 
         
